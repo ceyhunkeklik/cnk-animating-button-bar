f2239ce4062082ab1756b0458136fb8d3c03e474
Ceyhun Keklik <ceyhun.keklik@commencis.com>
2019-02-05 22:53:48 +0300

    initial commit

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > An animated button bar component for React
 
-[![NPM](https://img.shields.io/npm/v/cnk-animating-button-bar.svg)](https://www.npmjs.com/package/cnk-animating-button-bar) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![NPM](https://img.shields.io/npm/v/cnk-animating-button-bar.svg)](https://www.npmjs.com/package/cnk-animating-button-bar)
 
 ## Install
 
@@ -13,17 +13,77 @@ npm install --save cnk-animating-button-bar
 ## Usage
 
 ```tsx
-import * as React from 'react'
+import React, { Component } from "react";
 
-import MyComponent from 'cnk-animating-button-bar'
+import ButtonBar, {
+  MaterialDesignIcons as Icons
+} from "cnk-animating-button-bar";
 
-class Example extends React.Component {
-  render () {
+const buttons = [
+  {
+    icon: <Icons.MdLayers />,
+    text: "Home",
+    color: "#9C27B0"
+  },
+  {
+    icon: <Icons.MdFavoriteBorder />,
+    text: "Likes",
+    color: "#E91E63"
+  },
+  {
+    icon: <Icons.MdSearch />,
+    text: "Search",
+    color: "#FFC107"
+  },
+  {
+    icon: <Icons.MdPermIdentity />,
+    text: "Profile",
+    color: "#2196F3"
+  }
+];
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      panelIndex: -1
+    };
+  }
+
+  render() {
+    const self = this;
+    const { panelIndex } = self.state;
     return (
-      <MyComponent />
-    )
+      <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+        <div
+          style={{
+            flex: 1,
+            backgroundColor: "#F5F5F5",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          {panelIndex === -1 && "Welcome"}
+          {panelIndex === 0 && "Home"}
+          {panelIndex === 1 && "Likes"}
+          {panelIndex === 2 && "Search"}
+          {panelIndex === 3 && "Profile"}
+        </div>
+        <ButtonBar
+          buttons={buttons}
+          defaultIndex={panelIndex}
+          onSelectionChange={index => {
+            self.setState({ panelIndex: index });
+          }}
+          enableDeselect={true}
+        />
+      </div>
+    );
   }
 }
+
 ```
 
 ## License
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,204 @@
 /**
- * @class ExampleComponent
+ * @class ButtonBar
  */
 
-import * as React from 'react'
+import * as React from "react";
+import { style } from "typestyle";
+import classnames from "classnames";
 
-import styles from './styles.css'
+import * as Icons from "react-icons/md";
 
-export type Props = { text: string }
+export const MaterialDesignIcons = Icons;
 
-export default class ExampleComponent extends React.Component<Props> {
+const transitionTime = "0.5s";
+const defaultColor = "#212121";
+
+const styles = {
+  component: style({
+    padding: "20px 5px",
+    backgroundColor: "#FFFFFF",
+    display: "flex",
+    alignItems: "center",
+    userSelect: "none",
+    overflow: "hidden",
+    $nest: {
+      "& *": {
+        boxSizing: "border-box",
+        userSelect: "none"
+      }
+    }
+  }),
+  button: style({
+    flex: 1,
+    textAlign: "center",
+    display: "flex",
+    alignItems: "center",
+    cursor: "pointer",
+    userSelect: "none",
+    transition: `all ${transitionTime}`,
+    marginRight: 10,
+    $nest: {
+      "&:last-child": {
+        marginRight: 0
+      }
+    }
+  }),
+  buttonContent: style({
+    display: "flex",
+    alignItems: "center",
+    margin: "0 auto",
+    fontSize: 20,
+    whiteSpace: "nowrap",
+    transition: `all ${transitionTime}`,
+    padding: "10px 0",
+    borderRadius: 20,
+    maxWidth: 18,
+    overflow: "hidden",
+    $nest: {
+      "&.open": {
+        maxWidth: "100%",
+        padding: "10px 20px"
+      }
+    }
+  }),
+  iconWrapper: style({
+    transition: `width ${transitionTime}`
+  }),
+  textWrapper: style({
+    transition: `width ${transitionTime}`,
+    display: "inline-block",
+    fontSize: 14,
+    overflow: "hidden",
+    padding: "0 5px",
+    marginLeft: -20,
+    opacity: 0,
+    marginBottom: 4,
+    $nest: {
+      "&.open": {
+        opacity: 1,
+        marginLeft: 10
+      }
+    }
+  })
+};
+
+type Button = {
+  icon: any;
+  text: string;
+  color: string;
+};
+
+interface State {
+  buttons: Array<Button>;
+  selectedIndex: number;
+}
+
+export type Props = {
+  buttons: Array<Button>;
+  onSelectionChange: Function;
+  defaultIndex: number;
+  enableDeselect: boolean;
+};
+
+export default class ButtonBar extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      buttons: props.buttons,
+      selectedIndex:
+        !isNaN(props.defaultIndex) &&
+        props.defaultIndex > -1 &&
+        props.defaultIndex < props.buttons.length
+          ? props.defaultIndex
+          : -1
+    };
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.buttons !== this.props.buttons) {
+      this.setState({ buttons: this.props.buttons });
+    }
+  }
+
+  onButtonClick = (index: number) => {
+    const self = this;
+    const { selectedIndex } = self.state;
+
+    if (selectedIndex !== index || this.props.enableDeselect) {
+      const newIndex =
+        selectedIndex !== index
+          ? index
+          : this.props.enableDeselect
+          ? -1
+          : selectedIndex;
+      this.setState(
+        {
+          selectedIndex: newIndex
+        },
+        () => {
+          if (self.props.onSelectionChange) {
+            self.props.onSelectionChange(newIndex);
+          }
+        }
+      );
+    }
+  };
+
   render() {
-    const {
-      text
-    } = this.props
+    const self = this;
+    const { buttons, selectedIndex } = this.state;
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={styles.component}>
+        {buttons &&
+          buttons.map((item, index) => {
+            return (
+              <div
+                key={item.text}
+                className={styles.button}
+                onClick={() => {
+                  self.onButtonClick(index);
+                }}
+                style={{
+                  flexBasis:
+                    selectedIndex === index
+                      ? (100 / buttons.length) * 2
+                      : selectedIndex > -1
+                      ? (100 / buttons.length) * 0.75
+                      : 100 / buttons.length
+                }}
+              >
+                <div
+                  className={classnames({
+                    [styles.buttonContent]: true,
+                    open: selectedIndex === index
+                  })}
+                  style={{
+                    color:
+                      selectedIndex === index && item.color
+                        ? item.color
+                        : defaultColor,
+                    backgroundColor:
+                      selectedIndex === index && item.color
+                        ? item.color + "1E"
+                        : "transparent"
+                  }}
+                >
+                  <span className={styles.iconWrapper}>{item.icon}</span>
+                  <span
+                    className={classnames({
+                      [styles.textWrapper]: true,
+                      open: selectedIndex === index
+                    })}
+                  >
+                    {item.text}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
       </div>
-    )
+    );
   }
 }
